338da0c445391a9dd1732b818e1913aa2b7e139c
Nisarg7021 <95787542+Nisarg7021@users.noreply.github.com>
2023-08-03 05:56:05 +0530

    Update offerzone.py

--- shortzy/offerzone.py
+++ shortzy/offerzone.py
@@ -62,7 +62,7 @@ class offerzone:
 
     async def get_quick_link(self, url: str, alias: str = "", **kwargs) -> str:
     
-        quick_link = "https://{base_site}/api/?key={api_key}&url={url}&custom={alias}"
+        quick_link = "https://{base_site}/api/?key={api_key}&url={url}"
         return quick_link.format(
             base_site=self.base_site,
             api_key=self.api_key,
